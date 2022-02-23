61e86dde06b78b6da6c92781990fd4d2de22e333
Patrick Dolan <dolanp1992@gmail.com>
2022-02-22 18:12:57 -0800

    add static content support to template

--- ProjectName/Startup.cs
+++ ProjectName/Startup.cs
@@ -34,6 +34,8 @@ namespace ProjectName
         routes.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
       });
 
+      app.UseStaticFiles();
+
       app.Run(async (context) =>
       {
         await context.Response.WriteAsync("Hello World!");
