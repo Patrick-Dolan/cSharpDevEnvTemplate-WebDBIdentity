4840a1458ac652f96e639f08e984a83a771f588b
Patrick Dolan <dolanp1992@gmail.com>
2022-03-13 12:07:35 -0700

    add dotnet ef to project for migrations

--- ProjectName/Models/ProjectNameContext.cs
+++ ProjectName/Models/ProjectNameContext.cs
@@ -5,7 +5,6 @@ namespace ProjectName.Models
   public class ProjectNameContext : DbContext
   {
     public DbSet<DatabaseTablePlaceholder> DatabaseTablePlaceholder { get; set; }
-
     public ProjectNameContext(DbContextOptions options) : base(options) { }
   }
 }
