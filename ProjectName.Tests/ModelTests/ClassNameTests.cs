13173a03783a2605024ae8cf62e4e4bfb23abcef
Patrick Dolan <dolanp1992@gmail.com>
2022-03-28 17:32:13 -0700

    comment out tests until needed

--- ProjectName.Tests/ModelTests/ClassNameTests.cs
+++ ProjectName.Tests/ModelTests/ClassNameTests.cs
@@ -1,14 +1,14 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ProjectName.Models;
+// using Microsoft.VisualStudio.TestTools.UnitTesting;
+// using ProjectName.Models;
 
-namespace ProjectName.Tests
-{
-  [TestClass]
-  public class ClassNameTests
-  {
-    // Test methods go here
-  }
-}
+// namespace ProjectName.Tests
+// {
+//   [TestClass]
+//   public class ClassNameTests
+//   {
+//     // Test methods go here
+//   }
+// }
 // Example Test Method Structure
 //_________________________
 // [TestMethod]
